470f35434b938b761e78be23d4c516252b1e9dd6
a49955783-create <a49955783@gmail.com>
2025-09-11 00:53:42 +0300

    Update app.js
    NEW UPDATE

--- app.js
+++ app.js
@@ -250,7 +250,7 @@ dropArea.addEventListener('drop', e=>{
 
 /* ========== OCR ========== */
 async function runOCR(file){
-  dropArea.textContent = '... جاري استخراج النص ...';
+  dropArea.textContent = '... الرجاء الأنتظار ...';
   const mode = document.querySelector('input[name="mergeMode"]:checked')?.value || 'replace';
 
   try{
